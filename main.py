642ccef3a2772056e427474fa79ae0497eb776c3
EvilFlame112 <rampranav25@gmail.com>
2024-08-06 15:51:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from datetime import datetime
-
+import random
 filename = 'dokidoki.txt'
 
 with open(filename, 'a') as f:
     now = datetime.now()
     f.write(f'Today is {now.strftime("%Y-%m-%d")}\n')
-    f.write(f'This one is for you david\n')
+    l1 = ["David", "Lucy", "Rebecca", "Maine", "Jackie", "Dorio"]
+    f.write(f'This one is for you '+l1[random.randint(0,6)]+'\n')
